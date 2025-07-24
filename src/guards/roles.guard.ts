62ca1aa265eb915ff8c2e16e4f044e88bdf6cd05
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2025-07-23 21:21:01 -0300

    remove console.log

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -19,8 +19,6 @@ export class RolesGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const user = request.user;
 
-    console.log(user, user.acesso);
-
     if (!user || !requiredRoles.includes(user.acesso)) {
       throw new ForbiddenException('Acesso negado');
     }
